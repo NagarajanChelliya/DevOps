0a30522cf9caa68121286fe73344bfe00518fa67
Nagarajan <nagarajan.chelliya@gmail.com>
2025-09-27 22:36:20 +0100

    webhook

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 80;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from simple-app-Nagarajan-from Github-ci-cd\n');
+  res.end('Hello from simple-app-Nagarajan-CI-CD-Webhook\n');
 }).listen(port, () => console.log(`Running on ${port}`));
