f45e780597eb07fc34e8968944c0be80224263d9
Nagarajan <nagarajan.chelliya@gmail.com>
2025-09-27 17:22:26 +0100

    from github

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 80;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from simple-app-Nagarajan-latest\n');
+  res.end('Hello from simple-app-Nagarajan-from Github\n');
 }).listen(port, () => console.log(`Running on ${port}`));
