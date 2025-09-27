ffd89c287b34ada345644ab966125724eb886554
Nagarajan <nagarajan.chelliya@gmail.com>
2025-09-27 15:18:09 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const port = process.env.PORT || 80;
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from simple-app-Nagarajan-latest\n');
+}).listen(port, () => console.log(`Running on ${port}`));
